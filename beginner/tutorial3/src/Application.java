253520d79d043dd7a9f925c58cfeee8e48cb196b
shvayss <alex.shvayka@gmail.com>
2016-03-02 17:08:26 +0200

    working with strings

--- beginner/tutorial3/src/Application.java
+++ beginner/tutorial3/src/Application.java
@@ -5,6 +5,24 @@ public class Application
 {
     public static void main(String[] args)
     {
-        
+        int myInt = 7;
+
+        String text = "Hello";
+
+        String blank = " ";
+
+        String name = "Bob";
+
+        String greeting = text + blank + name;
+
+        System.out.println(greeting);
+
+        System.out.println("Hello" + " " + "Bobd");
+
+        System.out.println("My integer is: " + myInt);
+
+        double myDouble = 7.8;
+
+        System.out.println("My number is: " + myDouble);
     }
 }
