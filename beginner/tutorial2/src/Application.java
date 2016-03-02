bd4e8deae7a7a4b85338ea18c934779b3fdfd870
shvayss <alex.shvayka@gmail.com>
2016-03-02 14:56:46 +0200

    Added different variables for tutorial2

--- beginner/tutorial2/src/Application.java
+++ beginner/tutorial2/src/Application.java
@@ -1,8 +1,34 @@
 /**
  * Created by Alex.Shvayka on 3/2/2016.
  */
-public class Application {
-    public static void main(String[] args) {
+public class Application
+{
+    public static void main(String[] args)
+    {
+        int myNumber;
+        int myNum = 111;
+        short myShort = 847;
+        long myLong = 9797;
 
+        double myDouble = 7.3334;
+        float myFloat = 321.4f;
+
+        char myChar = 'y';
+        boolean myBoolean = false;
+
+        byte myByte = 127;
+
+        myNumber = 88;
+
+        System.out.println("Hello world!" + myNumber + " " + myNum);
+        System.out.println("My Number: " + myNumber);
+        System.out.println("My Num: " + myNum);
+        System.out.println("My Short: " + myShort);
+        System.out.println("My Long: " + myLong);
+        System.out.println("My Double: " + myDouble);
+        System.out.println("My Float: " + myFloat);
+        System.out.println("My Char: " + myChar);
+        System.out.println("My Boolean: " + myBoolean);
+        System.out.println("My Byte: " + myByte);
     }
 }
