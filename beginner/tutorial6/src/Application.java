f1819de9adb29dfc29c97f45bed459a201ba1fb8
shvayss <alex.shvayka@gmail.com>
2016-03-04 11:44:22 +0200

    added code changes for 'IF'

--- beginner/tutorial6/src/Application.java
+++ beginner/tutorial6/src/Application.java
@@ -5,6 +5,48 @@ public class Application
 {
     public static void main(String[] args)
     {
+        boolean cond = 1 < 6;
+        System.out.println(cond);
 
+        boolean cond1 = 1 > 6;
+        System.out.println(cond1);
+
+        boolean cond2 = 5 != 2;
+        System.out.println(cond2);
+
+        boolean cond3 = 4 == 3;
+        System.out.println(cond3);
+
+        if (4 == 4)
+        {
+            System.out.println("\n1. Yes, it's true!\n");
+        }
+
+        int myInt = 15;
+        if (myInt < 10)
+        {
+            System.out.println("2. Yes, it's true!");
+        }
+        else if (myInt > 20)
+        {
+            System.out.println("2. No, it's false!");
+        }
+        else
+        {
+            System.out.println("Non of the above.\n");
+        }
+
+        int loop = 0;
+        //while (loop < 5)
+        while (true)
+        {
+            System.out.println("Looping: " + loop);
+            if (loop == 5)
+            {
+                break;
+            }
+            loop++;
+            System.out.println("Running");
+        }
     }
 }
