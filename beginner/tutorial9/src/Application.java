5238280f49d84c24b319e94edfa5de6fffb5b452
shvayss <alex.shvayka@gmail.com>
2016-03-18 14:10:29 +0200

    added tutorial9 'Switch'

--- beginner/tutorial9/src/Application.java
+++ beginner/tutorial9/src/Application.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * Created by Alex.Shvayka on 3/17/2016.
  */
@@ -5,6 +7,21 @@ public class Application
 {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Please enter a command: ");
+        String text = input.nextLine();
+
+        switch (text)
+        {
+            case "start":
+                System.out.println("Machine started!");
+                break;
+            case "stop":
+                System.out.println("Machine stopped!");
+                break;
+            default:
+                System.out.println("Command not recognized!");
+        }
     }
 }
